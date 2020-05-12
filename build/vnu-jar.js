dca6c1ce41250aa1cfa4b4f24e43aea85abc66bc
XhmikosR <xhmikosr@gmail.com>
2020-05-12 10:30:43 +0300

    vnu.jar: ignore the error for now.

--- build/vnu-jar.js
+++ build/vnu-jar.js
@@ -36,7 +36,9 @@ childProcess.exec('java -version', (error, stdout, stderr) => {
     'The “time” input type is not supported in all browsers.*',
     // IE11 doesn't recognise <main> / give the element an implicit "main" landmark.
     // Explicit role="main" is redundant for other modern browsers, but still valid.
-    'The “main” role is unnecessary for element “main”.'
+    'The “main” role is unnecessary for element “main”.',
+    // This should only be temporary (hopefully)
+    'Element “style” not allowed as child of element “div” in this context.*'
   ].join('|')
 
   const args = [
